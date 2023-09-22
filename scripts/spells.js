d7adea66ae6ab1c240f1610117ba20ecbeaeecce
Shervorn <nrovrehs@yahoo.com>
2023-09-22 00:44:12 -0400

    Fix JSON object issue in spell page

--- scripts/spells.js
+++ scripts/spells.js
@@ -19,10 +19,20 @@ function createMonsterSheet(entry) {
   const casting_time = document.createElement("h2");
   const level = document.createElement("h2");
   const attack_type = document.createElement("h2");
+  const damage_type = document.createElement("h2");
   const damage = document.createElement("h2");
   const school = document.createElement("h2");
   const classes = document.createElement("h2");
   const subclasses = document.createElement("h2");
+
+  const damageSection = document.createElement("section");
+  damageSection.setAttribute("id", "damage")
+
+  const classesSection = document.createElement("section");
+  classesSection.setAttribute("id", "class")
+
+  const subclassSection = document.createElement("section");
+  subclassSection.setAttribute("id", "subclass")
 
   name.textContent = `Name: ${entry.name}`;
   desc.textContent = `Description: ${entry.desc}`;
@@ -36,10 +46,11 @@ function createMonsterSheet(entry) {
   casting_time.textContent = `Casting Time: ${entry.casting_time}`;
   level.textContent = `Level: ${entry.level}`;
   attack_type.textContent = `Attack Type: ${entry.attack_type}`;
-  damage.textContent = `Damage: ${entry.damage}`;
-  school.textContent = `School: ${entry.school}`;
-  classes.textContent = `Classes: ${entry.classes}`;
-  subclasses.textContent = `Subclasses: ${entry.subclasses}`;
+  damage.textContent = `Damage:`;
+  damage_type.textContent = `Damage Type: ${entry.damage.damage_type.name}`;
+  school.textContent = `School: ${entry.school.name}`;
+  classes.textContent = `Classes:`;
+  subclasses.textContent = `Subclasses:`;
 
   // img.src = `https://www.dnd5eapi.co${entry.image}`
 
@@ -56,16 +67,26 @@ function createMonsterSheet(entry) {
   section1.appendChild(casting_time);
   section1.appendChild(level);
   section1.appendChild(attack_type);
-  section1.appendChild(damage);
+  damageSection.appendChild(damage_type)
+  damageSection.appendChild(damage);
   section1.appendChild(school);
-  section1.appendChild(classes);
-  section1.appendChild(subclasses);
+  classesSection.appendChild(classes);
+  subclassSection.appendChild(subclasses);
 
   section.appendChild(section1);
   section.appendChild(section2);
   section.appendChild(section3);
   section.appendChild(section4);
   section.appendChild(section5);
+  section.appendChild(damageSection);
+  section.appendChild(classesSection);
+  section.appendChild(subclassSection);
+
+  console.log(entry.damage.damage_type.name)
+  console.log(entry.damage.damage_at_slot_level)
+  displayDamage(entry)
+  displayClasses(entry)
+  displaySubclasses(entry)
 }
 
 function loadMonsterList(monsters) {
@@ -117,3 +138,37 @@ fetchAPI("acid-arrow")
 // document.addEventListener('click', function(event) {
 //   console.log(event.target.textContent);
 // });
+
+function displayDamage(entry) {
+  const traits = entry.damage.damage_at_slot_level
+  const section = document.getElementById("damage");
+  console.log(traits)
+
+  for (const trait in traits) {
+    const listItems = document.createElement("h2");
+    listItems.textContent = `Level ${trait}: Damage ${traits[trait]}`;
+    section.appendChild(listItems);
+  }
+}
+
+function displayClasses(entry) {
+  const traits = entry.classes
+  const section = document.getElementById("class");
+
+  for (const trait of traits) {
+    const listItems = document.createElement("h2");
+    listItems.textContent = trait.name;
+    section.appendChild(listItems);
+  }
+}
+
+function displaySubclasses(entry) {
+  const traits = entry.subclasses
+  const section = document.getElementById("subclass");
+
+  for (const trait of traits) {
+    const listItems = document.createElement("h2");
+    listItems.textContent = trait.name;
+    section.appendChild(listItems);
+  }
+}
