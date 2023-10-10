384a7c1b86b57bdd18bcdb0448b2376e5e465d33
Shervorn <nrovrehs@yahoo.com>
2023-10-09 23:32:40 -0400

    Expand race page

--- scripts/races.js
+++ scripts/races.js
@@ -1,5 +1,8 @@
 function createMonsterSheet(entry) {
-    const section = document.querySelector("main")
+  const section = document.getElementById(entry.index);
+
+  var entryLength = section.childNodes.length
+  if (entryLength == 0) {
 
     const section1 = document.createElement("section");
     const section2 = document.createElement("section");
@@ -42,12 +45,6 @@ function createMonsterSheet(entry) {
     traits.textContent = `Traits:`;
     subraces.textContent = `Subraces: ${entry.subraces[0].name}`;
 
-    console.log(entry.starting_proficiencies)
-    console.log(entry.starting_proficiencies[0].name)
-
-    // img.src = `https://www.dnd5eapi.co${entry.image}`
-
-    // section1.appendChild(img)
     section1.appendChild(race);
     section1.appendChild(speed);
     section1.appendChild(ability_bonuses);
@@ -73,56 +70,49 @@ function createMonsterSheet(entry) {
     displayTraits(entry)
     displayLanguge(entry)
     displayProf(entry)
-}
-
-function loadMonsterList(monsters) {
-  const section = document.querySelector("section")
-
-  for (let entry = 0; entry < monsters.count; entry++) {
-    const listItem = document.createElement("h2")
-    listItem.textContent = monsters.results[entry].name
-    console.log(monsters.results[entry].name)
-    section.appendChild(listItem)
+  } else if (entryLength != 0) {
+    section.innerHTML = ""
   }
 }
 
-async function fetchAPI(input) {
+function loadMonsterList(monsters) {
+  const section = document.querySelector("main")
+
+  for (let entry = 0; entry < monsters.count; entry++) {
+    const listItem = document.createElement("h1")
+    listItem.textContent = monsters.results[entry].name
+    const monsterInfo = document.createElement("section")
+    monsterInfo.setAttribute("id", monsters.results[entry].index)
+    section.appendChild(listItem)
+    section.appendChild(monsterInfo)
+  }
+}
+
+async function fillRaceInfo(input) {
     const response = await fetch(`https://www.dnd5eapi.co/api/races/${input}`);
     const monster = await response.json();
-    console.log(monster);
 
     createMonsterSheet(monster)
 }
 
-fetchAPI("dwarf")
+async function fetchAPI() {
+  const response = await fetch(`https://www.dnd5eapi.co/api/races`);
+  const monsters = await response.json();
 
-// async function fetchAPI() {
-//   const response = await fetch(`https://www.dnd5eapi.co/api/races`);
-//   const monsters = await response.json();
-//   console.log(monsters);
-//   console.log(monsters.count);
+  loadMonsterList(monsters)
+}
 
-//   // console.log(monsters.results[monster].index);
+fetchAPI()
 
-//   loadMonsterList(monsters)
-// }
-
-// fetchAPI()
-
-// TO DO
-// Not all creatures have images + 
-// the ones that have images are AI generated
-// Find new source
-// Format page
-
-// The code below activates whenever you click on the page
-// Modify it to active the entry funtion whenever you click
-// An entries name
-// I'd like it to make a drop down
-
-// document.addEventListener('click', function(event) {
-//   console.log(event.target.textContent);
-// });
+document.addEventListener('click', function(event) {
+  var entry = event.target.textContent
+  entry = entry.toLowerCase()
+  entry = entry.replace(/ /g, "-")
+  entry = entry.replace(/\(|\)/g, "")
+  if (event.target.nodeName == "H1") {
+  fillRaceInfo(entry)
+  }
+});
 
 function displayTraits(entry) {
   const traits = entry.traits
